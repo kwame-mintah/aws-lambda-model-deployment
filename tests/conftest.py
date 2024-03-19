703ec1e5a1e6c0e19d840091e12daf8e0d7b859d
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-03-19 00:42:30 +0000

    docs(conftest): add comment to pytest fixture env var

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,8 @@ import model_deployment
 
 @pytest.fixture(autouse=True)
 def set_bucket_name(monkeypatch):
+    """
+    Stub bucket name for unit tests
+    :param monkeypatch:
+    """
     monkeypatch.setattr(model_deployment, "MODEL_OUTPUT_BUCKET_NAME", "unit-test")
